d0f1ddcca06438fceaf3a1141422f37b31320cf2
tterrag <tterrag1098@gmail.com>
2017-12-12 06:38:29 -0500

    Make ArrayPool a no-op

--- src/main/java/com/bulletphysics/util/ArrayPool.java
+++ src/main/java/com/bulletphysics/util/ArrayPool.java
@@ -37,9 +37,6 @@ import java.util.Map;
 public class ArrayPool<T> {
 
 	private Class componentType;
-	private ObjectArrayList list = new ObjectArrayList();
-	private Comparator comparator;
-	private IntValue key = new IntValue();
 	
 	/**
 	 * Creates object pool.
@@ -48,19 +45,6 @@ public class ArrayPool<T> {
 	 */
 	public ArrayPool(Class componentType) {
 		this.componentType = componentType;
-		
-		if (componentType == float.class) {
-			comparator = floatComparator;
-		}
-		else if (componentType == int.class) {
-			comparator = intComparator;
-		}
-		else if (!componentType.isPrimitive()) {
-			comparator = objectComparator;
-		}
-		else {
-			throw new UnsupportedOperationException("unsupported type "+componentType);
-		}
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -75,15 +59,10 @@ public class ArrayPool<T> {
 	 * @param length
 	 * @return array
 	 */
-	@SuppressWarnings("unchecked")
-	public T getFixed(int length) {
-		key.value = length;
-		int index = Collections.binarySearch(list, key, comparator);
-		if (index < 0) {
-			return create(length);
-		}
-		return (T)list.remove(index);
-	}
+    @SuppressWarnings("unchecked")
+    public T getFixed(int length) {
+        return create(length);
+    }
 
 	/**
 	 * Returns array that has same or greater length, or create one if not present
@@ -94,18 +73,7 @@ public class ArrayPool<T> {
 	 */
 	@SuppressWarnings("unchecked")
 	public T getAtLeast(int length) {
-		key.value = length;
-		int index = Collections.binarySearch(list, key, comparator);
-		if (index < 0) {
-			index = -index - 1;
-			if (index < list.size()) {
-				return (T)list.remove(index);
-			}
-			else {
-				return create(length);
-			}
-		}
-		return (T)list.remove(index);
+		return create(length);
 	}
 	
 	/**
@@ -114,58 +82,9 @@ public class ArrayPool<T> {
 	 * @param array previously obtained array from this pool
 	 */
 	@SuppressWarnings("unchecked")
-	public void release(T array) {
-		int index = Collections.binarySearch(list, array, comparator);
-		if (index < 0) index = -index - 1;
-		list.add(index, array);
-		
-		// remove references from object arrays:
-		if (comparator == objectComparator) {
-			Object[] objArray = (Object[])array;
-			for (int i=0; i<objArray.length; i++) {
-				objArray[i] = null;
-			}
-		}
-	}
+	public void release(T array) {}
 	
-	////////////////////////////////////////////////////////////////////////////
-
-	private static Comparator floatComparator = new Comparator() {
-		public int compare(Object o1, Object o2) {
-			int len1 = (o1 instanceof IntValue)? ((IntValue)o1).value : ((float[])o1).length;
-			int len2 = (o2 instanceof IntValue)? ((IntValue)o2).value : ((float[])o2).length;
-			return len1 > len2? 1 : len1 < len2 ? -1 : 0;
-		}
-	};
-
-	private static Comparator intComparator = new Comparator() {
-		public int compare(Object o1, Object o2) {
-			int len1 = (o1 instanceof IntValue)? ((IntValue)o1).value : ((int[])o1).length;
-			int len2 = (o2 instanceof IntValue)? ((IntValue)o2).value : ((int[])o2).length;
-			return len1 > len2? 1 : len1 < len2 ? -1 : 0;
-		}
-	};
-	
-	private static Comparator objectComparator = new Comparator() {
-		public int compare(Object o1, Object o2) {
-			int len1 = (o1 instanceof IntValue)? ((IntValue)o1).value : ((Object[])o1).length;
-			int len2 = (o2 instanceof IntValue)? ((IntValue)o2).value : ((Object[])o2).length;
-			return len1 > len2? 1 : len1 < len2 ? -1 : 0;
-		}
-	};
-	
-	private static class IntValue {
-		public int value;
-	}
-	
-	////////////////////////////////////////////////////////////////////////////
-	
-	private static ThreadLocal<Map> threadLocal = new ThreadLocal<Map>() {
-		@Override
-		protected Map initialValue() {
-			return new HashMap();
-		}
-	};
+	private static Map<Class, ArrayPool> map = new HashMap();
 	
 	/**
 	 * Returns per-thread array pool for given type, or create one if it doesn't exist.
@@ -174,9 +93,7 @@ public class ArrayPool<T> {
 	 * @return object pool
 	 */
 	@SuppressWarnings("unchecked")
-	public static <T> ArrayPool<T> get(Class cls) {
-		Map map = threadLocal.get();
-		
+	public static <T> ArrayPool<T> get(Class cls) {		
 		ArrayPool<T> pool = (ArrayPool<T>)map.get(cls);
 		if (pool == null) {
 			pool = new ArrayPool<T>(cls);
@@ -186,8 +103,6 @@ public class ArrayPool<T> {
 		return pool;
 	}
 
-	public static void cleanCurrentThread() {
-		threadLocal.remove();
-	}
+	public static void cleanCurrentThread() {}
 
 }
